ca5b9dd70ef3ce45f99e925b8c269be0f023f79f
Mohamed Abdelkader Zahana <mohamedashraf123@gmail.com>
2016-06-20 01:33:06 +0300

    fix test script

--- testScript.m
+++ testScript.m
@@ -3,16 +3,21 @@
 %% create object
 clear classes
 clc
+
+% create the object
 obj=ImgStream;
+%% set connection parameters
+obj.set_LocalPort(10000);
+obj.set_remoteHost('127.0.0.1')
 %% connect
+%1 if you want to display live images, 0 otherwise
 obj.showImage=1;
+% connect, and start receiving images
 obj.Connect();
 %% image data
 % image date is stored in 
 % obj.img
-% you can use that variable to manipulate the received image.
-%%
-obj.Disconnect();
-%% close/clean
+% you can use that variable to process the received image.
+%% close/clean object
 obj.Disconnect();
 obj.delete();
